1d212c8a5af25fc790a6c9b06a90defe7bafd2c1
Alessio Balsini <a.balsini@sssup.it>
2016-05-18 19:56:39 +0200

    Now it plots also the chain 3

--- examples/waters/PlotHistograms.m
+++ examples/waters/PlotHistograms.m
@@ -14,30 +14,45 @@ FF_1 = load("Chain_EffectChain_1_FF.txt") ./ unit;
 LL_1 = load("Chain_EffectChain_1_LL.txt") ./ unit;
 FF_2 = load("Chain_EffectChain_2_FF.txt") ./ unit;
 LL_2 = load("Chain_EffectChain_2_LL.txt") ./ unit;
+FF_3 = load("Chain_EffectChain_3_FF.txt") ./ unit;
+LL_3 = load("Chain_EffectChain_3_LL.txt") ./ unit;
 
 #FF_3 = load("Chain_EffectChain_3_FF.txt");
 #LL_3 = load("Chain_EffectChain_3_LL.txt");
 
-subplot(2,2,1)
+subplot(2,3,1)
 hist(FF_1, histResolution);
 title("Chain 1: First-to-Fist");
 xlabel(xLabelText);
 ylabel(yLabelText);
 
-subplot(2,2,2)
+subplot(2,3,2)
 hist(FF_2, histResolution);
 title("Chain 2: First-to-Fist");
 xlabel(xLabelText);
 ylabel(yLabelText);
 
-subplot(2,2,3)
+subplot(2,3,3)
+hist(FF_3, histResolution);
+title("Chain 3: First-to-Fist");
+xlabel(xLabelText);
+ylabel(yLabelText);
+
+subplot(2,3,4)
 hist(LL_1, histResolution);
 title("Chain 1: Last-to-Last");
 xlabel(xLabelText);
 ylabel(yLabelText);
 
-subplot(2,2,4)
+subplot(2,3,5)
 hist(LL_2, histResolution);
 title("Chain 2: Last-to-Last");
 xlabel(xLabelText);
 ylabel(yLabelText);
+
+subplot(2,3,6)
+hist(LL_3, histResolution);
+title("Chain 3: Last-to-Last");
+xlabel(xLabelText);
+ylabel(yLabelText);
+
